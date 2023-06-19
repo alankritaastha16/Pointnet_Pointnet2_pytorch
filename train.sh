69ab7bf73ddc4a36ba6d5f3bd6df86bf645f584e
alankritaastha16 <alankrita.astha16@gmail.com>
2023-06-19 16:51:07 +0200

    train command edited

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
-sbatch python3 train_partseg_iterativemodels.py --model PonderNet_part_seg_ssg --log_dir iterativenewmodels_part_seg_ssg  --epoch 100 --num_itr 1
-sbatch python3 train_partseg_iterativemodels.py --model PonderNet_part_seg_ssg --log_dir iterativenewmodels_part_seg_ssg  --epoch 100 --num_itr 2
-sbatch python3 train_partseg_iterativemodels.py --model PonderNet_part_seg_ssg --log_dir iterativenewmodels_part_seg_ssg  --epoch 100 --num_itr 3 
+sbatch python3 train_partseg_iterativemodels.py --model PonderNet_part_seg_ssg --log_dir iterativenewmodels_part_seg_ssg_numitr1  --epoch 100 --num_itr 1
+sbatch python3 train_partseg_iterativemodels.py --model PonderNet_part_seg_ssg --log_dir iterativenewmodels_part_seg_ssg_numitr2  --epoch 100 --num_itr 2
+sbatch python3 train_partseg_iterativemodels.py --model PonderNet_part_seg_ssg --log_dir iterativenewmodels_part_seg_ssg_numitr3  --epoch 100 --num_itr 3 

c79c471635eb7efc0cdcc9f378af741bb5d486ac
alankritaastha16 <alankrita.astha16@gmail.com>
2023-08-10 12:19:34 +0200

    new evals

---
+++ tteval.bat
@@ -0,0 +1,4 @@
+@echo off
+start /B python test_partseg_PonderNet.py --log_dir pointnetponderNet_part_seg_numitr3 --datadir "C:\Users\alank\OneDrive - TUM\Master Thesis\Algorithms\data" --num_itr 3
+start /B python test_partseg_PonderNet.py --log_dir pointnetponderNet_part_seg_numitr4 --datadir "C:\Users\alank\OneDrive - TUM\Master Thesis\Algorithms\data" --num_itr 4
+start /B python test_partseg_PonderNet.py --log_dir pointnetponderNet_part_seg_numitr5 --datadir "C:\Users\alank\OneDrive - TUM\Master Thesis\Algorithms\data" --num_itr 5

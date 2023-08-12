a213a778a3edd7e602acb5fef324946d070e5a47
alankritaastha16 <alankrita.astha16@gmail.com>
2023-08-12 21:46:02 +0200

    sbatchremoved

--- train3.sh
+++ train3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sbatch python3 train_partseg_iterativehidden_only_trainitr.py --model pointnetPondernethidden_part_seg --log_dir pointnetpondernetiterativehidden_part_seg_ssg_numitr1  --epoch 100 --num_itr 1 &
-sbatch python3 train_partseg_iterativehidden_only_trainitr.py --model pointnetPondernethidden_part_seg --log_dir pointnetpondernetiterativehidden_part_seg_ssg_numitr2  --epoch 100 --num_itr 2 &
-sbatch python3 train_partseg_iterativehidden_only_trainitr.py --model pointnetPondernethidden_part_seg --log_dir pointnetpondernetiterativehidden_part_seg_ssg_numitr3  --epoch 100 --num_itr 3 &
+python3 train_partseg_iterativehidden_only_trainitr.py --model pointnetPondernethidden_part_seg --log_dir pointnetpondernetiterativehidden_part_seg_ssg_numitr1  --epoch 100 --num_itr 1sbatch  &
+python3 train_partseg_iterativehidden_only_trainitr.py --model pointnetPondernethidden_part_seg --log_dir pointnetpondernetiterativehidden_part_seg_ssg_numitr2  --epoch 100 --num_itr 2 &
+python3 train_partseg_iterativehidden_only_trainitr.py --model pointnetPondernethidden_part_seg --log_dir pointnetpondernetiterativehidden_part_seg_ssg_numitr3  --epoch 100 --num_itr 3 &
 wait

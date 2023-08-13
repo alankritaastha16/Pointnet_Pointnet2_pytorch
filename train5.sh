2cc232812509b0df3b368880581a4abcbf23b104
alankritaastha16 <alankrita.astha16@gmail.com>
2023-08-13 19:00:00 +0200

    semsegpointnet added

---
+++ train5.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python3 train_semseg_ponderNethidden_only_trainitr.py --model pointnetPondernethidden_sem_seg --test_area 5 --log_dir pointnetPonderNet_sem_seg_num_itr1  --num_itr 1 &
+python3 train_semseg_ponderNethidden_only_trainitr.py --model pointnetPondernethidden_sem_seg --test_area 5 --log_dir pointnetPonderNet_sem_seg_num_itr2  --num_itr 2 &
+python3 train_semseg_ponderNethidden_only_trainitr.py --model pointnetPondernethidden_sem_seg --test_area 5 --log_dir pointnetPonderNet_sem_seg_num_itr3  --num_itr 3 &
+python3 train_semseg_ponderNethidden_only_trainitr.py --model pointnetPondernethidden_sem_seg --test_area 5 --log_dir pointnetPonderNet_sem_seg_num_itr4  --num_itr 4 &
+python3 train_semseg_ponderNethidden_only_trainitr.py --model pointnetPondernethidden_sem_seg --test_area 5 --log_dir pointnetPonderNet_sem_seg_num_itr5  --num_itr 5 &
+wait
